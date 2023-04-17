f1b7da972141b827893c402c1c28cf32d51631b4
satya2256 <87489600+satya2256@users.noreply.github.com>
2023-04-17 11:15:33 +0530

    --Hallticket Login implemented SD-4

--- College Project.Entities/UserStudent.cs
+++ College Project.Entities/UserStudent.cs
@@ -9,6 +9,7 @@ namespace College_Project.Entities
     {
     
         public string RollNumber { get; set; }
+        public string HallTicketNumber {get; set;}
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
