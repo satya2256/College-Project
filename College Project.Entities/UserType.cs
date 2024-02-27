7428ee5b32de323bbf6143f4b5f5e24face44d9a
satya2256 <87489600+satya2256@users.noreply.github.com>
2024-02-27 10:06:33 +0530

    SD-10808 Update UserType.cs

--- College Project.Entities/UserType.cs
+++ College Project.Entities/UserType.cs
@@ -8,6 +8,6 @@ namespace College_Project.Entities
     public class UserType:EntityBase
     {
         public string UserTypeName { get; set; }
-        //this is test for jenkins build
+        this is test for jenkins build
     }
 }
